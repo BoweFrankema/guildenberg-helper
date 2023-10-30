8e7ae5f74376c6c76691435127f08561e7cfaefa
Bowe Frankema <bowe@cfcommunity.net>
2023-10-30 16:51:46 +0100

    Add host images & config
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

---
+++ config/hosts.php
@@ -0,0 +1,109 @@
+<?php
+
+return [
+    'a2' => [
+        'name' => 'A2 Hosting',
+        'logo' => 'img/hosts/a2.png',
+        'custom_intro' => 'Welcome A2 customer.',
+    ],
+    'hostinger' => [
+        'name' => 'Hostinger',
+        'logo' => 'img/hosts/hostinger.png',
+        'custom_intro' => 'Welcome Hostinger customer.',
+    ],
+    'hostarmada' => [
+        'name' => 'HostArmada',
+        'logo' => 'img/hosts/hostarmada.png',
+        'custom_intro' => 'Welcome HostArmada customer.',
+    ],
+    'nestify' => [
+        'name' => 'Nestify',
+        'logo' => 'img/hosts/nestify.png',
+        'custom_intro' => 'Welcome Nestify customer.',
+    ],
+    'kinsta' => [
+        'name' => 'Kinsta',
+        'logo' => 'img/hosts/kinsta.png',
+        'custom_intro' => 'Welcome Kinsta customer.',
+    ],
+    'rocketnet' => [
+        'name' => 'Rocket.Net',
+        'logo' => 'img/hosts/rocketnet.png',
+        'custom_intro' => 'Welcome RocketNet customer.',
+    ],
+    'dreamhost' => [
+        'name' => 'DreamHost',
+        'logo' => 'img/hosts/dreamhost.png',
+        'custom_intro' => 'Welcome DreamHost customer.',
+    ],
+    'wpengine' => [
+        'name' => 'WPEngine',
+        'logo' => 'img/hosts/wpengine.png',
+        'custom_intro' => 'Welcome WPEngine customer.',
+    ],
+    'namecheap' => [
+        'name' => 'Namecheap',
+        'logo' => 'img/hosts/namecheap.png',
+        'custom_intro' => 'Welcome Namecheap customer.',
+    ],
+    'pressidium' => [
+        'name' => 'Pressidium',
+        'logo' => 'img/hosts/pressidium.png',
+        'custom_intro' => 'Welcome Pressidium customer.',
+    ],
+    'verpex' => [
+        'name' => 'Verpex',
+        'logo' => 'img/hosts/verpex.png',
+        'custom_intro' => 'Welcome Verpex customer.',
+    ],
+    'siteground' => [
+        'name' => 'SiteGround',
+        'logo' => 'img/hosts/siteground.png',
+        'custom_intro' => 'Welcome SiteGround customer.',
+    ],
+    'cloudways' => [
+        'name' => 'Cloudways',
+        'logo' => 'img/hosts/cloudways.png',
+        'custom_intro' => 'Welcome Cloudways customer.',
+    ],
+    'greengeeks' => [
+        'name' => 'GreenGeeks',
+        'logo' => 'img/hosts/greengeeks.png',
+        'custom_intro' => 'Welcome GreenGeeks customer.',
+    ],
+    'convesio' => [
+        'name' => 'Convesio',
+        'logo' => 'img/hosts/convesio.png',
+        'custom_intro' => 'Welcome Convesio customer.',
+    ],
+    'krystal' => [
+        'name' => 'Krystal',
+        'logo' => 'img/hosts/krystal.png',
+        'custom_intro' => 'Welcome Krystal customer.',
+    ],
+    'yourhostingnl' => [
+        'name' => 'YourHosting NL',
+        'logo' => 'img/hosts/yourhostingnl.png',
+        'custom_intro' => 'Welcome YourHosting NL customer.',
+    ],
+    'inmotionhosting' => [
+        'name' => 'InMotion Hosting',
+        'logo' => 'img/hosts/inmotionhosting.png',
+        'custom_intro' => 'Welcome InMotion Hosting customer.',
+    ],
+    'wpx' => [
+        'name' => 'WPX Hosting',
+        'logo' => 'img/hosts/wpx.png',
+        'custom_intro' => 'Welcome WPX Hosting customer.',
+    ],
+    'nexcess' => [
+        'name' => 'Nexcess',
+        'logo' => 'img/hosts/nexcess.png',
+        'custom_intro' => 'Welcome Nexcess customer.',
+    ],
+    'pantheon' => [
+        'name' => 'Pantheon',
+        'logo' => 'img/hosts/pantheon.png',
+        'custom_intro' => 'Welcome Pantheon customer.',
+    ],
+];

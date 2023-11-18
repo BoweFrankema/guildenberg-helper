95a17dcb3c87a14cb71cea8d569f7fb0d312dadc
Bowe Frankema <bowe@cfcommunity.net>
2023-11-18 12:58:30 +0100

    Remove CPT logic. Will be in v2
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- guildenberg-helper.php
+++ guildenberg-helper.php
@@ -14,7 +14,7 @@ class Guildenberg_Helper_Plugin
     {
         $this->host_data = $this->get_normalized_gb_host_by_ref();
         // Hook into WordPress 'init' action to register the custom post type
-        add_action("init", [$this, "create_gb_hosts_cpt"]);
+
 
         // Add settings page for the plugin
         add_action("admin_menu", [$this, "add_plugin_page"]);
@@ -174,109 +174,7 @@ class Guildenberg_Helper_Plugin
         ';
     }
 
-    public function create_gb_hosts_cpt()
-    {
-        $labels = [
-            "name" => _x("GB Hosts", "Post type general name", "textdomain"),
-            "singular_name" => _x(
-                "GB Host",
-                "Post type singular name",
-                "textdomain"
-            ),
-            "menu_name" => _x("GB Hosts", "Admin Menu text", "textdomain"),
-            "name_admin_bar" => _x(
-                "GB Host",
-                "Add New on Toolbar",
-                "textdomain"
-            ),
-            "add_new" => __("Add New", "textdomain"),
-            "add_new_item" => __("Add New GB Host", "textdomain"),
-            "new_item" => __("New GB Host", "textdomain"),
-            "edit_item" => __("Edit GB Host", "textdomain"),
-            "view_item" => __("View GB Host", "textdomain"),
-            "all_items" => __("All GB Hosts", "textdomain"),
-            "search_items" => __("Search GB Hosts", "textdomain"),
-            "parent_item_colon" => __("Parent GB Hosts:", "textdomain"),
-            "not_found" => __("No GB Hosts found.", "textdomain"),
-            "not_found_in_trash" => __(
-                "No GB Hosts found in Trash.",
-                "textdomain"
-            ),
-            "featured_image" => _x(
-                "GB Host Featured Image",
-                "Overrides the “Featured Image” phrase for this post type. Added in 4.3",
-                "textdomain"
-            ),
-            "set_featured_image" => _x(
-                "Set featured image",
-                "Overrides the “Set featured image” phrase for this post type. Added in 4.3",
-                "textdomain"
-            ),
-            "remove_featured_image" => _x(
-                "Remove featured image",
-                "Overrides the “Remove featured image” phrase for this post type. Added in 4.3",
-                "textdomain"
-            ),
-            "use_featured_image" => _x(
-                "Use as featured image",
-                "Overrides the “Use as featured image” phrase for this post type. Added in 4.3",
-                "textdomain"
-            ),
-            "archives" => _x(
-                "GB Host archives",
-                "The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4",
-                "textdomain"
-            ),
-            "insert_into_item" => _x(
-                "Insert into GB Host",
-                "Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4",
-                "textdomain"
-            ),
-            "uploaded_to_this_item" => _x(
-                "Uploaded to this GB Host",
-                "Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4",
-                "textdomain"
-            ),
-            "filter_items_list" => _x(
-                "Filter GB Hosts list",
-                "Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4",
-                "textdomain"
-            ),
-            "items_list_navigation" => _x(
-                "GB Hosts list navigation",
-                "Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4",
-                "textdomain"
-            ),
-            "items_list" => _x(
-                "GB Hosts list",
-                "Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4",
-                "textdomain"
-            ),
-        ];
 
-        $args = [
-            "labels" => $labels,
-            "public" => false,
-            "publicly_queryable" => false,
-            "show_ui" => true,
-            "show_in_menu" => true,
-            "query_var" => true,
-            "rewrite" => ["slug" => "gb-hosts"],
-            "capability_type" => "post",
-            "has_archive" => false,
-            "hierarchical" => false,
-            "menu_position" => null,
-            "supports" => [
-                "title",
-                "editor",
-                "thumbnail",
-                "custom-fields",
-                "slug",
-            ],
-        ];
-
-        register_post_type("gb_hosts", $args);
-    }
 
     public function gb_host_name_shortcode($atts)
     {
